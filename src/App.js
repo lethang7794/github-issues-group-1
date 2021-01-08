670836594dd8be136368e6b85fcc68914aef9d86
nngocjade <nngocjade@gmail.com>
2021-01-08 18:30:23 +0700

    added navBar styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
+import IssueList from "./components/IssueList";
+import SearchForm from "./components/SearchForm";
+import NavBar from "./components/NavBar";
 
 function App() {
+  const [issues, setIssues] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [url, setUrl] = useState(
+    "https://api.github.com/repos/octocat/hello-world/issues"
+  );
+  // const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      let data = await fetch(url);
+      let json = await data.json();
+      setIssues(json);
+    }
+    fetchData();
+  }, [url]); //url here is a dependency
+
+  const handleClick = () => {
+    setUrl(`https://api.github.com/repos/${searchTerm}/issues`);
+  };
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
-    <Container>
-      <h1 className="text-center">GitHub Issues Browser</h1>
-      <p>Fill in files from /src/components.</p>
-    </Container>
+    <div className="App">
+      <NavBar />
+      <Container>
+        <h1 className="text-center title">GitHub Issues Browser</h1>
+        <SearchForm
+          handleChange={handleChange}
+          handleClick={handleClick}
+          searchTerm={searchTerm}
+        />
+        <IssueList issues={issues} />
+      </Container>
+    </div>
   );
 }
 
