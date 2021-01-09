0d8046e78cc01f7540306667241cbd5237d6309d
nngocjade <nngocjade@gmail.com>
2021-01-09 11:34:16 +0700

    styled searchForm UI

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const SearchForm = ({ handleChange, searchTerm, handleClick }) => {
   return (
-    <div>
+    <div className="search-form-wrapper">
       <input type="text" onChange={handleChange} value={searchTerm} />
       <button onClick={handleClick}>Search</button>
     </div>
