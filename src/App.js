681544e91d61b79c92f47355a5348090eec80de1
Thang Le Quang <lethang7794@gmail.com>
2021-01-09 14:42:33 +0700

    Add basic ErrorMessage component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import IssueList from "./components/IssueList";
 import SearchForm from "./components/SearchForm";
+import ErrorMessage from "./components/ErrorMessage";
 
 function App() {
   const [issues, setIssues] = useState([]);
@@ -12,20 +13,33 @@ function App() {
     "https://api.github.com/repos/octocat/hello-world/issues"
   );
 
+  const [isError, setIsError] = useState(false);
+  // Can't pass down to ErrorMessage component the whole response/result object.
+  // Can only pass the error type, status, status text, url as an array.
+  const [error, setError] = useState([]);
+
   useEffect(() => {
     async function fetchData() {
       try {
+        // Reset isError state, later if there is an error, set isError to true.
+        setIsError(false);
+
         let response = await fetch(url);
         let result = await response.json();
 
+        console.log(response);
         if (response.ok) {
           setIssues(result);
         } else {
           console.log(
-            'There is an error. We still receive a response from the server. But it says: "Not Found!" ...'
+            'There is an error. We still receive a response from the server. But it says somethings, e.g. "Not Found!"'
           );
+          setIsError(true);
+          setError(["response", response.status, response.statusText]);
         }
       } catch (error) {
+        setIsError(true);
+        setError(["fetch"]);
         console.log(
           "There is an error. e.g. No internet. We don't receive any response from the server."
         );
@@ -51,6 +65,9 @@ function App() {
         handleClick={handleClick}
         searchTerm={searchTerm}
       />
+
+      {isError && <ErrorMessage error={error} />}
+
       <IssueList issues={issues} />
     </Container>
   );
